5c7dd3fb7348d538011490dc1b6dcec4a4c13abf
Nwachukwu Chisom <samsondestined@gmail.com>
2024-10-29 00:15:00 +0100

    forgot-passpord debug

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Forgot your password? No problem. Chisom, don\'t tell me you\'ve forgotten your password. Well, just let me know your email address and I will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
     <!-- Session Status -->
