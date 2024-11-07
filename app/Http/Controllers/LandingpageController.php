2c9f7b52beca809d1683435e0e411094da2a38c9
Nwachukwu Chisom <samsondestined@gmail.com>
2024-11-07 16:42:10 +0100

    Laravel Telescope implemented

--- app/Http/Controllers/LandingpageController.php
+++ app/Http/Controllers/LandingpageController.php
@@ -13,7 +13,6 @@ class LandingpageController extends Controller
         $hire_section = Hire::get();
         $testimony_section = Testimony::query()->orderBy('id', 'desc')->get();
 
-
         return view('dashboard')
             ->with('hire_section', $hire_section)
             ->with('testimony_section', $testimony_section);
